8c9957a0e9e2466557645a30793f5bc28b72a1cc
louis0320 <zldqjrj@naver.com>
2023-03-07 16:38:24 +0900

    #10 - repository interface 작성
    이걸로 db에 접근

---
+++ project-board/src/main/java/com/portfolio/projectboard/repository/ArticleCommentRepository.java
@@ -0,0 +1,7 @@
+package com.portfolio.projectboard.repository;
+
+import com.portfolio.projectboard.domain.ArticleComment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+}
